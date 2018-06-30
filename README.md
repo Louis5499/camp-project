3fa833644d504ebd6816b2312750bb57d82735a1
louis_Chiang <louis@giloo.ist>
2018-06-30 18:56:44 +0800

    Add babel & element UI

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 NTHU Summer Camp APP
 
 教學大地 ， 串接 firebase
+
+## How to run dev?
+npm run serve
+
+## How to run build?
+npm run build
--- src/main.js
+++ src/main.js
@@ -2,10 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
 import firebase from 'firebase'
+
+// Element UI
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.config.productionTip = false
 
+// Firebase initialize
 var config = {
   apiKey: "AIzaSyCFrCY90QeV_F2vqI4ZNpCmtPu3TxttJ28",
   authDomain: "camp-project-ac382.firebaseapp.com",
@@ -15,6 +21,9 @@ var config = {
   messagingSenderId: "470156386455"
 };
 firebase.initializeApp(config);
+
+// Element UI initialize
+Vue.use(ElementUI);
 
 new Vue({
   router,
