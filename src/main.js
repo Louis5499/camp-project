97d5e496f2c5f3e8ef67b5c06ebeedb24c3fd1fe
louis_Chiang <louis@giloo.ist>
2018-06-30 18:32:04 +0800

    Initialize firebase

--- src/main.js
+++ src/main.js
@@ -2,8 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+var config = {
+  apiKey: "AIzaSyCFrCY90QeV_F2vqI4ZNpCmtPu3TxttJ28",
+  authDomain: "camp-project-ac382.firebaseapp.com",
+  databaseURL: "https://camp-project-ac382.firebaseio.com",
+  projectId: "camp-project-ac382",
+  storageBucket: "",
+  messagingSenderId: "470156386455"
+};
+firebase.initializeApp(config);
 
 new Vue({
   router,
