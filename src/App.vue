6ef9b22fc89023ac033852d42e82ad24baf875b7
louis_Chiang <louis@giloo.ist>
2018-07-02 23:00:45 +0800

    add

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,7 @@
 </template>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Roboto+Condensed');
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/views/Main.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="mainWrapper">
+    <div class="topWrapper">
+      <div class="moneyWrapper">
+        <div class="moneyTop">
+          <img class="bitcoin" src="../assets/bitcoin.png" width="20" height="20">
+          <span class="bitcoinTitle">Bitcoin</span>
+        </div>
+        <span class="bitcoinAmount">2300</span>
+      </div>
+      <div class="cardWrapper">
+        <div class="atkWrap">
+          <span class="cardNum">7</span>
+        </div>
+        <div class="defWrap">
+          <span class="cardNum">3</span>
+        </div>
+        <div class="spWrap">
+          <span class="cardNum">6</span>
+        </div>
+      </div>
+    </div>
+    <div class="scoreBoard">
+        <div class="boardTitle">
+          <div class="titleRank">排名</div>
+          <div class="titleTeam">組別</div>
+          <div class="titleMoney">金錢</div>
+        </div>
+        <div class="boardContent">
+          <div class="contentRank">排名</div>
+          <div class="contentTeam">組別</div>
+          <div class="contentMoney">金錢</div>
+        </div>
+      </div>
+  </div>
+</template>
+
+<script>
+import css from '../css/main.scss'
+export default {
+  
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
